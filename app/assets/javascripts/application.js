559bc99e6ab1025ed2b22075ba55d24b2a4b0178
尾沼亮太 <ryota@onumaryoutanoMacBook-Air.local>
2019-05-21 10:11:52 +0900

    Update9

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-//= require Chart.min 
+//= require Chart.min
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
